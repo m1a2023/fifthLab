74b8487b319a350e9d1b7af563c941f205aa556e
m1a <mag2017go@gmail.com>
2023-12-17 00:27:45 +0500

    Initial commit

---
+++ fifthLab/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace fifthLab;
+
+class Program
+{
+    static void Main()
+    {
+        Number num1 = new Number(5);
+        Number num2 = new Number(9);
+        Number result = num1 + num2;
+        Console.WriteLine(result.Value);
+    }
+}
---
+++ fifthLab/classFifthLab.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace fifthLab
+{
+    class Token
+    {
+        //Number, operation and parenthesis
+        public string token { get; set; }
+        
+        public void Print()
+        {
+            Console.WriteLine(token.ToString());
+        }
+    }
+
+    class Number : Token
+    {
+        //here is numbers
+        public double Value { get; set; }
+        public Number(double value)
+        {
+            Value = value;
+        }
+        public static Number operator +(Number x, Number y)
+        {
+            return new Number(x.Value + y.Value);
+        }
+        public static Number operator -(Number x, Number y)
+        {
+            return new Number(x.Value - y.Value);
+        }
+        public static Number operator *(Number x, Number y)
+        {
+            return new Number(x.Value * y.Value);
+        }
+        public static Number operator /(Number x, Number y)
+        {
+            if (y.Value != 0)
+            {
+                return new Number(x.Value / y.Value);
+            }
+            else
+            {
+                throw new ArgumentException("Division by zero!");
+            }
+        }
+        
+    }
+
+    class Operation : Token
+    {
+        //here is +, -, *, /
+        public char operation { get; set; }
+        public int priority;
+
+        private int Priority(char operation)
+        {
+            if ("+-*/".Contains(operation))
+            {
+                return priority = operation switch
+                {
+                    '*' => 2,
+                    '/' => 2,
+                    '-' => 1,
+                    '+' => 1,
+                };
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect operation!");
+            }
+        }
+    }
+
+    class Parenthesis : Token
+    {
+        // here is '(' or ')'
+        public char bracket { get; set; }
+        public bool isOpen;
+        private bool Transparency(char bracket)
+        {
+            return isOpen = bracket switch
+            {
+                '(' => true,
+                ')' => false
+            };
+        }
+    }
+}
